4a577ee2cf04d1e664e12c8c2a8a1179d32afa19
Christopher Miller <git@chrismiller.xyz>
2025-07-27 18:45:21 -0400

    Comment canRon function and fixed the furiten test

--- src/statefunctions/decisionfunction.cc
+++ src/statefunctions/decisionfunction.cc
@@ -14,28 +14,39 @@
 namespace mahjong {
 
 // TODO(#18): "I really hate this" - alice
-bool CanRon(const GameState& state,
-            int player) {
+bool CanRon(const GameState& state, int player) {
+
+  // If the pending piece is your disables, you can't Ron
   for (const auto& piece : state.hands.at(player).discards) {
     if (state.pendingPiece == piece) {
       return false;
     }
   }
+
+  // Build the theoretical hand
   auto& tmp_state = const_cast<GameState&>(state);
   tmp_state.hands.at(player).live.push_back(state.pendingPiece);
   tmp_state.hands.at(player).sort();
+
+  // If this Ron is occurring due to a concealed kan discard,
   if (state.concealedKan) {
+    // If it happens to be a ron for a thirteen orphans,
+    // it's allowed and you can ron
     if (isThirteenOrphans(state, player) != 0) {
       tmp_state.hands.at(player).live.erase(
           std::find(state.hands.at(player).live.begin(),
                     state.hands.at(player).live.end(), state.pendingPiece));
       return true;
     }
+
+    // otherwise, you can't
     tmp_state.hands.at(player).live.erase(
         std::find(state.hands.at(player).live.begin(),
                   state.hands.at(player).live.end(), state.pendingPiece));
     return false;
   }
+
+  // if not a concealed kan, check if it's complete
   const bool can_ron = isComplete(state, player);
   tmp_state.hands.at(player).live.erase(
       std::find(state.hands.at(player).live.begin(),
--- tests/game_play.test.cc
+++ tests/game_play.test.cc
@@ -10,11 +10,13 @@
 
 namespace mahjong {
 
-TEST(GamePlay, DISABLED_Furiten) {
+TEST(GamePlay, Furiten) {
   GameState state;
   state.pendingPiece = kRedDragon;
   state.hands[0].live = {kRedDragon, kRedDragon, kRedDragon};
   ASSERT_TRUE(isComplete(state, 0));
+  // Place Red Dragon in Discard Pile
+  state.hands[0].discards = {kRedDragon};
   state.hands[0].live = {kRedDragon, kRedDragon};
   EXPECT_FALSE(CanRon(state, 0));
 }
