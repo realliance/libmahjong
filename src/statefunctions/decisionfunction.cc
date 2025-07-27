fa51d284ca75967f06fb7505025c1bfff6736e6b
Christopher Miller <git@chrismiller.xyz>
2025-07-27 18:47:54 -0400

    Fix new lines

--- src/statefunctions/decisionfunction.cc
+++ src/statefunctions/decisionfunction.cc
@@ -15,7 +15,6 @@ namespace mahjong {
 
 // TODO(#18): "I really hate this" - alice
 bool CanRon(const GameState& state, int player) {
-
   // If the pending piece is your disables, you can't Ron
   for (const auto& piece : state.hands.at(player).discards) {
     if (state.pendingPiece == piece) {
